24726e57c0638216f9e4d9a9c22fc9bdaa4abec9
Jorge Restrepo <jarestrepogu@gmail.com>
2025-08-07 19:22:27 -0500

    wip: add placeholder widget

---
+++ RecurlyChallengeApp/AppWeaterWidget/AppWeaterWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  AppWeaterWidgetBundle.swift
+//  AppWeaterWidget
+//
+//  Created by Jorge Restrepo on 7/08/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct AppWeaterWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        AppWeaterWidget()
+    }
+}
